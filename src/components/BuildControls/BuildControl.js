b462f8ef0729950bf65a68df47b1ccc085d47efb
Nikson Rotondaro <nikson_rotondaro@hotmail.com>
2021-06-28 23:26:05 -0300

    add and remove ingredients implemented

--- src/components/BuildControls/BuildControl.js
+++ src/components/BuildControls/BuildControl.js
@@ -1,11 +1,19 @@
-import classes from './BuildControl.module.css';
+import classes from "./BuildControl.module.css";
 
 const BuildControl = (props) => (
-    <div className={classes.BuildControl}>
-        <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
-    </div>
-)
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button
+      className={classes.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
+    <button className={classes.More} onClick={props.added}>
+      More
+    </button>
+  </div>
+);
 
 export default BuildControl;
--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -1,19 +1,25 @@
-import classes from './BuildControls.module.css';
-import BuildControl from './BuildControl';
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl";
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'chesse'},
-    {label: 'Meat', type: 'meat'},
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
 ];
 
 const BuildControls = (props) => (
-        <div className={classes.BuildControls}>
-            {controls.map(ctrl => (
-                <BuildControl key={ctrl.label} label={ctrl.label} />
-            ))}
-        </div>
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl) => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+  </div>
 );
 
 export default BuildControls;
